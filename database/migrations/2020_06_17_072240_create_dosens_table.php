7ee3f8ed0cb0f47321d0ba80b63a5e277b03ce7d
Libran hadi saputra <libranmen@gmail.com>
2020-06-17 00:32:25 -0700

    make migration & controller for dosen

---
+++ database/migrations/2020_06_17_072240_create_dosens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+        
+            $table->string('name');
+            $table->string('matkul');
+            $table->string('address');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+}
